ecf9c0aea5775519b4c78b21f3e694d78cb8a7b4
백민수 <iphonex@kakao.com>
2022-02-07 20:22:46 +0900

    - 로그인 관련 수정

--- src/main/java/com/example/communitywebservice/security/WebSecurity.java
+++ src/main/java/com/example/communitywebservice/security/WebSecurity.java
@@ -25,13 +25,13 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
-//        http.authorizeRequests().antMatchers("/users/**").permitAll()
-        http.authorizeRequests().antMatchers("/users/**")
-                        .hasIpAddress("172.30.1.56")
-                        .and()
-                        .addFilter(getAuthenticationFilter());
-
-        http.headers().frameOptions().disable();
+        http.authorizeRequests().antMatchers("/users/**").permitAll();
+//        http.authorizeRequests().antMatchers("/users/**")
+//                        .hasIpAddress("172.30.1.56")
+//                        .and()
+//                        .addFilter(getAuthenticationFilter());
+//
+//        http.headers().frameOptions().disable();
     }
 
     private AuthenticationFilter getAuthenticationFilter() throws Exception {
--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT INTO USER_INFO(ID, EMAIL, ENCRYPTED_PWD, NAME, USER_ID)
-VALUES (1, 'minsuback@naver.com', '$2a$10$KBfqoClm0DcEI/hM3cIoL.46DJS2nz3mTl.A00oRD7IHslyxheWyi', '백민수', 'minsuback');
+INSERT INTO USER_INFO(ID, EMAIL, ENCRYPTED_PWD, NAME, USER_ID, USER_NO)
+VALUES (1, 'minsuback@naver.com', '$2a$10$KBfqoClm0DcEI/hM3cIoL.46DJS2nz3mTl.A00oRD7IHslyxheWyi', '백민수', 'minsuback', '1');
